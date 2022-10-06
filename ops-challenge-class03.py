0d53f211b1b050067c853ba906af57f5e4062afd
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-10-05 19:49:30 -0500

    Update ops-challenge-class03.py

--- ops-challenge-class03.py
+++ ops-challenge-class03.py
@@ -8,16 +8,15 @@ import smtplib
 user_ip = input("Please enter IP address: ") 
 user_email = input("Please enter your email address: ")
 user_password = input("Please enter your email password: ")
-current_output = None
-last_output = None
+current_ping = 0
+last_ping = 0
 
 def ping():
     response = os.system("ping -n 1 " + user_ip)
     if response == 0:
-        print(datetime.datetime.now(), user_ip, 'is up')
+        return True
     else:
-        print(datetime.datetime.now(), user_ip, 'is down')
-    time.sleep(2)
+        return False
 
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.ehlo()
@@ -25,24 +24,22 @@ server.ehlo()
 server.login(user_email, user_password)
 
 def send_upAlert():
-    datetime.datetime.now()
+    print(datetime.datetime.now())
     msg = "Server is up!"
     server.sendmail('flyhomes123@gmail', user_email, msg)
     server.quit()
     
 def send_downAlert():
-    datetime.datetime.now()
+    print(datetime.datetime.now())
     msg = "Server is down!"
     server.sendmail('flyhomes123@gmail', user_email, msg)
     server.quit()
   
 while True:
-    current_output == ping()
-    if current_output == last_output:
-        if current_output == True:
+    current_ping == ping()
+    if current_ping != last_ping:
+        if current_ping == True:
             send_upAlert()
-        if current_output == False:
+        if current_ping == False:
             send_downAlert()
-    last_output = current_output
-    
-# Got it to send an email but it's always downAlert  
+    last_ping = current_ping 
