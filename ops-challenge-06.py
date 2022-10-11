2a1d4019ec504ecea7c5b4333d84013f06fe06e9
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-10-11 11:55:47 -0500

    Update ops-challenge-06.py

--- ops-challenge-06.py
+++ ops-challenge-06.py
@@ -1,31 +1,12 @@
 #!/usr/bin/python3
 
 
-# still working on this one
+# still working on this script
 
 
 from cryptography.fernet import Fernet
 
-# infinite while loop
-ans=True
-while ans:
-    # creates list
-    print ("""
-    1. Encrypt a file
-    2. Decrypt a file
-    3. Encrypt a message
-    4. Decrpyt a message
-    """)
-    # asks for user input
-    ans=raw_input("What would you like to do? ") 
-    if ans=="1" or ans=="2":
-    # creates file path variable 
-      user_path = input(print("\n Please provide file path: "))
-    elif ans=="3" or ans=="4":
-    # creates message variable
-      user_msg = input(print("\n Please provide a message: "))
-    else:
-      print("\n Please try again") 
+# key generation
 
 # generates key and saves it to file
 def write_key():
@@ -39,19 +20,61 @@ def load_key():
 
 write_key()
     
+f = Fernet(key)
+
 key = load_key()
 print("Key is " + str(key.decode('utf-8')))
 
-message = user_msg.encode()
-print("Plaintext is " + str(message.decode('utf-8')))
+# functions
 
-f = Fernet(key)
+def encrypt_file():
+    file = user_path.encode()
+    encrypted_file = f.encrypt(file)
+    print("File is " + str(encrypted_file.decode('utf-8')))
+  
+def decrypt_file():
+    file = user_path.encode()
+    decrypted_file = f.decrypt(file)
+    print("File is " + str(decrypted_file.decode('utf-8')))
+    
+def encrypt_message():
+    message = user_msg.encode()
+    encrypted_message = f.encrypt(message)
+    print("Ciphertext is " + str(encrypted_message.decode('utf-8')))
+    
+def decrypt_message():
+    message = user_msg.encode()
+    decrypted_message = f.decrypt(message)
+    print("Plaintext is " + str(decrypted_message.decode('utf-8')))
 
-# Encrypt message
-encrypted = f.encrypt(message)
-# Print encrpyted message
-print("Ciphertext is " + str(encrypted.decode('utf-8')))
 
-
-decrypted = f.decrypt(encrypted)
-print("Decrypted message is " + str(decrypted))
+# infinite while loop
+ans=True
+while ans:
+    # creates list
+    print ("""
+    1. Encrypt a file
+    2. Decrypt a file
+    3. Encrypt a message
+    4. Decrypt a message
+    """)
+    # asks for user input
+    ans=raw_input("What would you like to do? ") 
+    if ans=="1":
+        user_path = input(print("\n Please provide file path: "))
+        encrypt_file()
+        
+    elif ans=="2":
+        user_path = input(print("\n Please provide file path: "))
+        decrypt_file()
+        
+    elif ans=="3":
+        user_msg = input(print("\n Please provide a message: "))
+        encrypt_message()
+    
+    elif ans=="4":
+        user_msg = input(print("\n Please provide a message: "))
+        decrypt_message()
+        
+    else:
+      print("\n Please try again") 
