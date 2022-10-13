c1ad756746581038ade638439e91a040fe5fec93
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-10-12 21:17:00 -0500

    Update ops-challenge-08.py

--- ops-challenge-08.py
+++ ops-challenge-08.py
@@ -2,8 +2,11 @@
 
 import ctypes
 
+# replaces background, WIP
 ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 3)
 
+
+# generates popup window
 MessageBox = ctypes.windll.user32.MessageBoxW
 res = MessageBox(None, 'Transfer $5 million USD worth of XMR or your data will be compromised', 'All Your Base Are Belong To Us', 0)
 print(res)
