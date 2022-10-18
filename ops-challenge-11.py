428083fee89a04c4c76542e5e1de3401e8b2b2c4
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-10-17 22:51:07 -0500

    Update ops-challenge-11.py

--- ops-challenge-11.py
+++ ops-challenge-11.py
@@ -13,11 +13,17 @@ port_range = [22, 23, 80, 443, 3389]
 
 for port in port_range:
   
-  resp =
+  resp = ls port_range
  
-  if resp = None
-    print(
-  elif resp = TCP
-      
-  elif = resp = ICMP
+  if resp = None:
+    print(host + resp, " is filtered")
+  
+  elif resp = TCP:
+    if resp = sys.flag "0x12"
+    print(host + resp, " is open")
+    elif resp = sys.flag "0x14"
+    print(host + resp, " is closed")
+ 
+  elif resp = ICMP:
+    print(host + resp, " is silently dropped)
   
