5d21ef94343c530baa5159cd9fa82ec5c84b33d9
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-10-17 22:28:18 -0500

    Create ops-challenge-11.py

---
+++ ops-challenge-11.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# WIP
+
+import sys
+import scapy
+from sys import flags
+from scapy.all import sr,sr1,IP,TCP,ICMP
+
+host = input("Please enter IP address: ")
+
+port_range = [22, 23, 80, 443, 3389]
+
+for port in port_range:
+  
+  resp =
+ 
+  if resp = None
+    print(
+  elif resp = TCP
+      
+  elif = resp = ICMP
+  
