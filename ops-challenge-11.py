a41213712c8b949f37186316468b2458a6744139
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-10-18 23:47:00 -0500

    Update ops-challenge-11.py

--- ops-challenge-11.py
+++ ops-challenge-11.py
@@ -2,28 +2,32 @@
 
 # WIP
 
-import sys
-import scapy
 from sys import flags
 from scapy.all import sr,sr1,IP,TCP,ICMP
+import random
 
-host = input("Please enter IP address: ")
+host=input("Please enter IP address: ")
+port_range=[22,23,80,443,3389]
 
-port_range = [22, 23, 80, 443, 3389]
+for dst_port in port_range:
+  src_port = random.randint(1025,65534)
+    resp = sr1(
+        IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=1,
+        verbose=0,
+    )
+    
+    if resp is None:
+        print(f"{host}:{dst_port} is filtered (silently dropped).")
+    
+    elif(resp.haslayer(TCP)):
+        if(resp.getlayer(TCP).flags == 0x12):
+            send_rst = sr(
+                IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags='R'),
+                timeout=1,
+                verbose=0,
+            )
+            print(f"{host}:{dst_port} is open.")
 
-for port in port_range:
-  
-  resp = ls port_range
- 
-  if resp = None:
-    print(host + resp, " is filtered")
-  
-  elif resp = TCP:
-    if resp = sys.flag "0x12"
-    print(host + resp, " is open")
-    elif resp = sys.flag "0x14"
-    print(host + resp, " is closed")
- 
-  elif resp = ICMP:
-    print(host + resp, " is silently dropped)
+        elif (resp.getlayer(TCP).flags == 0x14):
+            print(f"{host}:{dst_port} is closed.")
   
