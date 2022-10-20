f647fd50ed61ab1df5beeab9a805d32c01c459ae
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-10-20 01:00:42 -0500

    Update ops-challenge-12.py

--- ops-challenge-12.py
+++ ops-challenge-12.py
@@ -1,75 +1,58 @@
 #!/usr/bin/python3
 
-# WIP
-
-
-import sys  
-from sys import flags
-from scapy.all import ICMP, IP, sr1, sr, TCP
+import sys
+from scapy.all import ICMP,IP,sr1,sr,TCP
 import random
 from ipaddress import IPv4Network
-from datetime import datetime
 
-print ("""
+def option_menu():
+    print ("""
     1. TCP Port Range Scanner
     2. ICMP Ping Sweeper
     3. Exit
     """)
-    # asks for user input
-    ans=raw_input("Please choose an option: ") 
+    ans=input("Please choose an option: ") 
     if ans=="1":
-        port_scan()
-        
+        port_scan()  
     elif ans=="2":
-        ping_sweep()
-        
+        ping_sweep()   
     elif ans=="3":
-        sys.exit()
-        
+        sys.exit()   
     else:
-      print("\n Please try again")
+      print("Please try again")
  
 
 def port_scan():
   remote_server=input("Please enter an IP address to scan: ")
   remote_serverIP = socket.gethostbyname(remote_server)
   port_range=[22,23,80,443,3389]
-  
-  try:
-    for port in port_range:
-      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      result = sock.connect_ex((remote_serverIP, port))
-      if result ==0:
-        print "Port {}:        Open".format(port)
-        sock.close()
-        
-        except KeyboardInterrupt:
-          print("You pressed Ctrl+C")
-          sys.exit()
-        
-        except socket.gaierror:
-          print("Hostname could not be resolved. Exiting")
-          sys.exit()
-          
-        except socket.error:
-          print("Couldn't connect to server")
-          sys.exit()
-
-     
-def ping_sweep():
-  ip_address=input("Please enter IP address to ping: ")
-  ip_list=IPv4(ip_address)
-  print(ip_list)
-  
-  for ip in ip_list:
-  
-    elif(resp.haslayer(ICMP)):
-        if(
-            int(resp.getlayer(ICMP).type) == 3 and
-            int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]
-        ):
+  for dst_port in port_range:
+        src_port = random.randint(1025, 65534)
+        resp = sr1(IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=1,verbose=0)
+        if resp is None: 
             print(f"{host}:{dst_port} is filtered (silently dropped).")
-  
-  
-  
+        elif (resp.haslayer(TCP)):
+            if(resp.getlayer(TCP).flags==0x12):
+                print(f"{host}:{dst_port} is open.")
+            elif(resp.getlayer(TCP).flags==0x14):
+                print(f"{host}:{dst_port} is closed.")
+   
+def ping_sweep():
+  ip_address=input("Please enter IP address in CIDR notation to ping: ")
+  ip_list=IPv4(ip_address)
+  live_count=0
+  for ip in ip_list:
+        resp = sr1(IP(dst=str(host))/ICMP(),timeout=2,verbose=0,)
+        if (host in (ipadd.network_address,ipadd.broadcast_address)):
+            continue
+        if resp is None:
+            print(f"{host} is not responding.")
+        elif (int(resp.getlayer(ICMP).type)==3 and int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+            print(f"{host} is blocking ICMP.")
+        else:
+            print(f"{host} is responding.")
+            live_count += 1
+
+while True:
+    option_menu()
   
