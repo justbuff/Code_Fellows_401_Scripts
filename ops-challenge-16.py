86529242debf48f45175f769d6aeb5ecc8db4779
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-10-24 23:20:52 -0500

    Create ops-challenge-16.py

---
+++ ops-challenge-16.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# WIP
+
+# Automated Brute Force Wordlist Attack Tool Part 1 of 3
+
+# Mode 1: Offensive; Dictionary Iterator
+
+# Accepts a user input word list file path and iterates through the word list, assigning the word being read to a variable.
+# Add a delay between words.
+# Print to the screen the value of the variable.
+
+# Mode 2: Defensive; Password Recognized
+
+# Accepts a user input string.
+# Accepts a user input word list file path.
+# Search the word list for the user input string.
+# Print to the screen whether the string appeared in the word list.
+
+
+import time
+import getpass
+
+def iterator():
+  path = input("Please enter your dictionary filepath:\n")
+  file = open(path)
+  line = file.readline()
+  print(line)
+  
+  while line:
+    line = line.rstrip()
+    word = line
+    print(word)
+    time.sleep(1)
+    line = file.readline()
+  file.close()
+  
+def password_check():
+  string = input("Please enter a string:\n")
+  path = input("Please enter the wordlist file path:\n")
+  if string in path:
+    print(str(string) + " is inside the wordlist")
+  else:
+    print(str(string) + " is not inside the wordlist")
+  
+iterator()
+password_check()
+  
+
