695334973712a5a79c24ad1ca6fec67247db0e73
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-11-09 22:32:04 -0600

    Update ops-challenge-27.py

--- ops-challenge-27.py
+++ ops-challenge-27.py
@@ -1,45 +1,31 @@
 #!/usr/bin/env python3
 
-import logging
-import time
-from logging.handlers import TimedRotatingFileHandler
 import os
+import logging
 import datetime
+import time
+from logging.handlers import RotatingFileHandler
 
+logger = logging.getLogger('logger_example')
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler('file.log')
+logger.addHandler(handler)
 
 def check_ping():
+    print("Logging Started")
+    logging.debug("Start Log")
     response = os.system("ping -n 1 8.8.8.8") 
     if response == 0: 
         print(datetime.datetime.now(), '8.8.8.8 is up')
+        logging.info("8.8.8.8 is up")
     else:
         print(datetime.datetime.now(), '8.8.8.8 is down')
-    time.sleep(2) 
-    logging.basicConfig(filename='example.txt', encoding='utf-8', level=logging.DEBUG)
-    print("Logging Started")
-    logging.debug("Debug Info")
-    logging.info("Info")
-    logging.warning("Warning Info")
-    logging.error("Error Info")
-    logging.critical("Critical Info")
+        logging.error("8.8.8.8 is down")
+    time.sleep(2)
     print("Logging End")
-    
-def create_timed_rotating_log(path):
-    logger = logging.getLogger("Rotating Log")
-    logger.setLevel(logging.INFO)
-    
-    handler = TimedRotatingFileHandler(path,
-                                       when="h",
-                                       interval=1,
-                                       backupCount=5)
-    logger.addHandler(handler)
-    
-    for i in range(6):
-        logger.info("This is a test!")
-        time.sleep(75)
 
-if __name__ == "__main__":
-    log_file = "timed_test.log"
-    create_timed_rotating_log(log_file)
+if __name__ == '__main__':
+    check_ping()
   
 
 
