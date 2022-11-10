aab1d36df3497937e097db9c02b506488f7e122d
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-11-09 22:24:09 -0600

    Update ops-challenge-26.py

--- ops-challenge-26.py
+++ ops-challenge-26.py
@@ -5,24 +5,20 @@ import logging
 import datetime
 import time
 
+logging.basicConfig(filename='26log.txt', encoding='utf-8', level=logging.DEBUG)
 
 def check_ping():
+    print("Logging Started")
+    logging.debug("Start Log")
     response = os.system("ping -n 1 8.8.8.8") 
     if response == 0: 
         print(datetime.datetime.now(), '8.8.8.8 is up')
+        logging.info("8.8.8.8 is up")
     else:
         print(datetime.datetime.now(), '8.8.8.8 is down')
-    time.sleep(2) 
-  
-logging.basicConfig(filename='example.txt', encoding='utf-8', level=logging.DEBUG)
-print("Logging Started")
-logging.debug("Debug Info")
-logging.info("Info")
-logging.warning("Warning Info")
-logging.error("Error Info")
-logging.critical("Critical Info")
-print("Logging End")
+        logging.error("8.8.8.8 is down")
+    time.sleep(2)
+    print("Logging End")
 
 if __name__ == '__main__':
     check_ping()
-
