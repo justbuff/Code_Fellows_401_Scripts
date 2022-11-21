2301339e53ff904d134846be4493b56f46eaa37e
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-11-21 10:39:48 -0600

    Update ops-challenge-31.py

--- ops-challenge-31.py
+++ ops-challenge-31.py
@@ -13,15 +13,18 @@ def linux_search():
   os.system("ls " + str(prompt_file) + " | echo \"$(wc -l) files searched.\"")
 
 def windows_search():
-  $prompt_file = Read-Host -Prompt "Please provide a target fille path"
-  $prompt_directory = Read-Host -Prompt "Please provide a target directory path"
-  function Search-Directory {
-    Get-ChildItem -Path $prompt_directory -Filter $prompt_file
-  }
-  Write-Output Search-Directory
+  prompt_file = input("Please provide a target file path: ")
+  prompt_directory = input("Please provide a target directory path: ")
+  for root, dir, files in os.walk(prompt_directory):
+      if prompt_file in files:
+         result.append(os.path.join(root, prompt_file))
+  print(result)
+  os.system("ls " + str(prompt_file) + " | echo \"$(wc -l) files searched.\"")
+
+def platform_search():
+  if platform =="Linux":
+    linux_search()
+  elif platform == "win32":
+    windows_search()
   
-if platform =="linux" or "platform" = "linux2":
-  linux_search()
-elif platform == "win32":
-  windows_search()
-  
+platform_search()
