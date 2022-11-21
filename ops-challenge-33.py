8f689a70566792d18d5f4dfd03392d4f0835756b
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-11-21 13:31:39 -0600

    Update ops-challenge-33.py

--- ops-challenge-33.py
+++ ops-challenge-33.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
+# to be combined with ops challenges 31 and 32
 
-https://github.com/eduardxyz/virustotal-search/blob/master/virustotal-search.py
-  
+from sys import platform
+import os, time, datetime, math
+from matplotlib.pyplot import pause
+import hashlib
+
+def malware_check():
+    api_key = os.getenv('API_KEY_VIRUSTOTAL') 
+    query = 'python3 virustotal-search.py -k ' + api_key + ' -m ' + hash
+    os.system(query)
   
