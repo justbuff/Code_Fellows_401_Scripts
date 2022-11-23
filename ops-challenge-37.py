1f4ddbe48c6bede9950540be65e1d10f6e60232e
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-11-22 18:18:09 -0600

    Update ops-challenge-37.py

--- ops-challenge-37.py
+++ ops-challenge-37.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import requests
+import webbrowser
+import urllib.request
 
 # targetsite = input("Enter target site:") # Uncomment this to accept user input target site
 targetsite = "http://www.whatarecookies.com/cookietest.asp" # Comment this out if you're using the line above
@@ -29,8 +31,18 @@ print(cookie)
 
 # Add here some code to make this script perform the following:
 # - Send the cookie back to the site and receive a HTTP response
+
+requests.post(targetsite, cookies=cookie)
+response
+
 # - Generate a .html file to capture the contents of the HTTP response
+
+urllib.request.urlretrieve(response, "test.txt")
+
 # - Open it with Firefox
+
+webbrowser.open_new_tab("test.txt")
+
 #
 # Stretch Goal
 # - Give Cookie Monster hands
